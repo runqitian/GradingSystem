fd2714777120288704503f6988a6ff2b2d5c3dcf
mhendrick98 <mhendric@bu.edu>
2019-04-29 11:35:44 -0400

    to be merged

--- src/main/java/core/DatabaseStorage.java
+++ src/main/java/core/DatabaseStorage.java
@@ -1,18 +1,9 @@
 package core;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ConnectionString;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientURI;
 import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.model.CreateCollectionOptions;
-import com.mongodb.client.model.ValidationOptions;
 import org.bson.Document;
 
 import java.util.ArrayList;
@@ -23,13 +14,13 @@ public class DatabaseStorage {
 
     // Connects to mongo instance
     public static MongoDatabase connectToDB(){
-        MongoClient mongoClient = MongoClients.create();
-        MongoDatabase database = mongoClient.getDatabase("591Test");
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://test:testing1@ds135624.mlab.com:35624/cs591"));
+        MongoDatabase database = mongoClient.getDatabase("cs591");
         return database;
     }
 
     // Allows users to sign up - might not but necessary for what we are doing
-    public void signUp(String user, String pass){
+    public static void signUp(String user, String pass){
         MongoDatabase db = connectToDB();
         MongoCollection<Document> collection = db.getCollection("testAccounts");
         Document newDocument = new Document("username", user).append("password", pass);
@@ -37,18 +28,18 @@ public class DatabaseStorage {
     }
 
     // Allows user to login. Returns true if the login was successful or false otherwise
-    public Boolean logIn(String user, String pass){
+    public static Boolean logIn(String user, String pass){
         MongoDatabase db = connectToDB();
         MongoCollection<Document> collection = db.getCollection("testAccounts");
         Document possibleMatch = collection.find(eq("username", user)).first();
-        if((possibleMatch != null) && (possibleMatch.get("password") == pass)){
+        if((possibleMatch != null) && (possibleMatch.get("password").equals(pass))){
             return true;
         }
         return false;
     }
 
     // Adds a new category to the database. Based off the design found here: https://docs.google.com/document/d/1Eq5BSkV4JUp511XVhcvLPc3SBE5irwBFiqChFFtwCpQ/edit
-    public void addSubCategory (String name, Double gradWeight, Double ugradWeight, ArrayList<SubCategory> subCategories, String categoryBelongTo, Double maxPossible, HashMap<String, Double> grades){
+    public static void addSubCategory (String name, Double gradWeight, Double ugradWeight, ArrayList<SubCategory> subCategories, String categoryBelongTo, Double maxPossible, HashMap<String, Double> grades){
         MongoDatabase db = connectToDB();
         MongoCollection<Document> collection = db.getCollection("testCategories");
         Document newDocument = new Document("name", name).append("gradWeight", gradWeight).append("uGradWeight", ugradWeight).append("subCategories", subCategories.toArray()).append("categoryBelongTo", categoryBelongTo).append("maxPossible", maxPossible).append("grades", grades);
@@ -57,7 +48,7 @@ public class DatabaseStorage {
 
 
     // Fetches a category entry from the database.
-    public ArrayList findSubCategory(String name, String categoryBelongTo){
+    public static ArrayList findSubCategory(String name, String categoryBelongTo){
         MongoDatabase db = connectToDB();
         MongoCollection<Document> collection = db.getCollection("testCategories");
         Document possibleMatch = collection.find(eq("name", name)).first();
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,17 +2,23 @@ package main;
 
 import controllers.Controller;
 import core.GradingSystem;
+import core.SubCategory;
 import gui.MainFrame;
 import core.DatabaseStorage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        GradingSystem gradingSystem = new GradingSystem();
-        MainFrame mainFrame = new MainFrame();
+//        GradingSystem gradingSystem = new GradingSystem();
+//        MainFrame mainFrame = new MainFrame();
 //        Controller controller = new Controller(mainFrame, gradingSystem);
 //        mainFrame.test();
         DatabaseStorage.connectToDB();
+        DatabaseStorage.addSubCategory("assignment1",30.00,10.00,new ArrayList<SubCategory>(),"hw",100.00,new HashMap<String, Double>());
+        System.out.println(DatabaseStorage.findSubCategory("assignment1", "hw"));
 
     }
 
