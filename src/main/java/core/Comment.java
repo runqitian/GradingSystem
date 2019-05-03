003bd95fb2396a8e02d52e19e89b68a23a8d1920
GentleXu <47066141+GentleXu@users.noreply.github.com>
2019-05-03 04:38:45 -0400

    comment

--- src/main/java/core/Comment.java
+++ src/main/java/core/Comment.java
@@ -1,5 +1,11 @@
 package core;
 
+import java.util.HashMap;
+import java.util.Map;
+//import java.util.Vector;
+
 public class Comment {
+    Map<String, String> comment = new HashMap<String, String>() {};
+    
 
 }
